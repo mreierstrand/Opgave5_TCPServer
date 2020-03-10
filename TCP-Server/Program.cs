09d1eb41ce705574ef44ec159dbfe8478635b51f
Oliver Eierstrand <oliv1115@edu.easj.dk>
2020-03-10 14:10:04 +0100

    Opgave5 Done

--- TCP-Server/Program.cs
+++ TCP-Server/Program.cs
@@ -1,13 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
-
+using Newtonsoft.Json;
 namespace TCP_Server
 {
     class Program
     {
+
+        private static readonly List<Book.Book> books = new List<Book.Book>()
+        {
+            new Book.Book("Løvernes Konge","JK Rowling",100,"1234567891011"),
+            new Book.Book("Harry Potter","Omar Masouk",200,"1234567891012")
+        };
+
+
         static void Main(string[] args)
         {
             IPAddress ip = IPAddress.Parse("192.168.24.205");
@@ -37,12 +48,37 @@ namespace TCP_Server
             string message = sr.ReadLine();
             string answer = "";
 
+
             while (message != null && message != "")
             {
-                Console.WriteLine("GET ALL" + message);
-                answer = message.ToUpper();
-                sw.WriteLine(answer);
+                string[] messageArray = message.Split(' ');
+                string param = message.Substring(message.IndexOf(' ') + 1);
+                string command = messageArray[0];
+
+                switch (command)
+                {
+                    case "GetAll":
+                        sw.WriteLine("Get all recieved");
+                        sw.WriteLine(JsonConvert.SerializeObject(books));
+                        break;
+
+                    case "Get":
+                        sw.WriteLine("Get request recieved - Isbn " + messageArray[1] + "requested");
+                        sw.WriteLine(JsonConvert.SerializeObject(books.Find(id=>id.ISBN13 == param)));
+                        break;
+
+                    case "Save":
+                        sw.WriteLine("Save recieved");
+                        Book.Book saveBook = JsonConvert.DeserializeObject<Book.Book>(param);
+                        books.Add(saveBook);
+                        break;
+
+                    default:
+                       sw.WriteLine("Den er gal");
+                        break;
+                }
                 message = sr.ReadLine();
+
             }
 
             ns.Close();
